e12717304e6ec8c2e9f58479fb014e493af7a811
Brandon <68867795+ZipBrandon@users.noreply.github.com>
2024-08-22 09:13:59 -0400

    Adding ProviderComponent to NodeRender

--- packages/react/src/container/NodeRenderer/index.tsx
+++ packages/react/src/container/NodeRenderer/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, Fragment } from 'react';
 import { shallow } from 'zustand/shallow';
 
 import { useVisibleNodeIds } from '../../hooks/useVisibleNodeIds';
@@ -8,6 +8,7 @@ import { GraphViewProps } from '../GraphView';
 import { useResizeObserver } from './useResizeObserver';
 import { NodeWrapper } from '../../components/NodeWrapper';
 import type { Node, ReactFlowState } from '../../types';
+import type { ReactNode } from 'react;
 
 export type NodeRendererProps<NodeType extends Node> = Pick<
   GraphViewProps<NodeType>,
@@ -25,7 +26,7 @@ export type NodeRendererProps<NodeType extends Node> = Pick<
   | 'nodeExtent'
   | 'nodeTypes'
   | 'nodeClickDistance'
->;
+> & {ProviderComponent?: ReactNode};
 
 const selector = (s: ReactFlowState) => ({
   nodesDraggable: s.nodesDraggable,
@@ -39,59 +40,61 @@ function NodeRendererComponent<NodeType extends Node>(props: NodeRendererProps<N
   const { nodesDraggable, nodesConnectable, nodesFocusable, elementsSelectable, onError } = useStore(selector, shallow);
   const nodeIds = useVisibleNodeIds(props.onlyRenderVisibleElements);
   const resizeObserver = useResizeObserver();
-
+  const ProviderComponent = props.ProviderComponent || Fragment;
   return (
     <div className="react-flow__nodes" style={containerStyle}>
-      {nodeIds.map((nodeId) => {
-        return (
-          // The split of responsibilities between NodeRenderer and
-          // NodeComponentWrapper may appear weird. However, it’s designed to
-          // minimize the cost of updates when individual nodes change.
-          //
-          // For example, when you’re dragging a single node, that node gets
-          // updated multiple times per second. If `NodeRenderer` were to update
-          // every time, it would have to re-run the `nodes.map()` loop every
-          // time. This gets pricey with hundreds of nodes, especially if every
-          // loop cycle does more than just rendering a JSX element!
-          //
-          // As a result of this choice, we took the following implementation
-          // decisions:
-          // - NodeRenderer subscribes *only* to node IDs – and therefore
-          //   rerender *only* when visible nodes are added or removed.
-          // - NodeRenderer performs all operations the result of which can be
-          //   shared between nodes (such as creating the `ResizeObserver`
-          //   instance, or subscribing to `selector`). This means extra prop
-          //   drilling into `NodeComponentWrapper`, but it means we need to run
-          //   these operations only once – instead of once per node.
-          // - Any operations that you’d normally write inside `nodes.map` are
-          //   moved into `NodeComponentWrapper`. This ensures they are
-          //   memorized – so if `NodeRenderer` *has* to rerender, it only
-          //   needs to regenerate the list of nodes, nothing else.
-          <NodeWrapper<NodeType>
-            key={nodeId}
-            id={nodeId}
-            nodeTypes={props.nodeTypes}
-            nodeExtent={props.nodeExtent}
-            onClick={props.onNodeClick}
-            onMouseEnter={props.onNodeMouseEnter}
-            onMouseMove={props.onNodeMouseMove}
-            onMouseLeave={props.onNodeMouseLeave}
-            onContextMenu={props.onNodeContextMenu}
-            onDoubleClick={props.onNodeDoubleClick}
-            noDragClassName={props.noDragClassName}
-            noPanClassName={props.noPanClassName}
-            rfId={props.rfId}
-            disableKeyboardA11y={props.disableKeyboardA11y}
-            resizeObserver={resizeObserver}
-            nodesDraggable={nodesDraggable}
-            nodesConnectable={nodesConnectable}
-            nodesFocusable={nodesFocusable}
-            elementsSelectable={elementsSelectable}
-            nodeClickDistance={props.nodeClickDistance}
-            onError={onError}
-          />
-        );
-      })}
+      <ProviderComponent>
+        {nodeIds.map((nodeId) => {
+          return (
+            // The split of responsibilities between NodeRenderer and
+            // NodeComponentWrapper may appear weird. However, it’s designed to
+            // minimize the cost of updates when individual nodes change.
+            //
+            // For example, when you’re dragging a single node, that node gets
+            // updated multiple times per second. If `NodeRenderer` were to update
+            // every time, it would have to re-run the `nodes.map()` loop every
+            // time. This gets pricey with hundreds of nodes, especially if every
+            // loop cycle does more than just rendering a JSX element!
+            //
+            // As a result of this choice, we took the following implementation
+            // decisions:
+            // - NodeRenderer subscribes *only* to node IDs – and therefore
+            //   rerender *only* when visible nodes are added or removed.
+            // - NodeRenderer performs all operations the result of which can be
+            //   shared between nodes (such as creating the `ResizeObserver`
+            //   instance, or subscribing to `selector`). This means extra prop
+            //   drilling into `NodeComponentWrapper`, but it means we need to run
+            //   these operations only once – instead of once per node.
+            // - Any operations that you’d normally write inside `nodes.map` are
+            //   moved into `NodeComponentWrapper`. This ensures they are
+            //   memorized – so if `NodeRenderer` *has* to rerender, it only
+            //   needs to regenerate the list of nodes, nothing else.
+            <NodeWrapper<NodeType>
+              key={nodeId}
+              id={nodeId}
+              nodeTypes={props.nodeTypes}
+              nodeExtent={props.nodeExtent}
+              onClick={props.onNodeClick}
+              onMouseEnter={props.onNodeMouseEnter}
+              onMouseMove={props.onNodeMouseMove}
+              onMouseLeave={props.onNodeMouseLeave}
+              onContextMenu={props.onNodeContextMenu}
+              onDoubleClick={props.onNodeDoubleClick}
+              noDragClassName={props.noDragClassName}
+              noPanClassName={props.noPanClassName}
+              rfId={props.rfId}
+              disableKeyboardA11y={props.disableKeyboardA11y}
+              resizeObserver={resizeObserver}
+              nodesDraggable={nodesDraggable}
+              nodesConnectable={nodesConnectable}
+              nodesFocusable={nodesFocusable}
+              elementsSelectable={elementsSelectable}
+              nodeClickDistance={props.nodeClickDistance}
+              onError={onError}
+            />
+          );
+        })}
+      </ProviderComponent>
     </div>
   );
 }
